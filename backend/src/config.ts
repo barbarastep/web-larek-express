bac02186b84287a7e0cd32a4f49bab4af1d68589
Barbara Step <b.barbarastep@gmail.com>
2026-02-15 20:50:30 +0300

    fix: load environment variables using dotenv

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,2 +1,6 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const DB_ADDRESS = process.env.DB_ADDRESS || 'mongodb://127.0.0.1:27017/weblarek';
 export const PORT = process.env.PORT || 3000;
